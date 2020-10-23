edd99feb6256e1153273335ed5daef4f8f37b3e6
lukevoss <luke@familievoss.net>
2020-10-23 11:29:45 +0200

    boxplot for each attributess

--- Practica 3/practica3.m
+++ Practica 3/practica3.m
@@ -1,6 +1,30 @@
 close all
 clear all
 
-A = importdata('wine.data');
-meanArray = kmeans(A, 13);
-plot(meanArray,'o')
+%analizar el número óptimo de clústeres
+Wines = importdata('wine.data');
+Data = Wines(:,2:14);
+%for i = 1:size(Data,2) %columnas
+%    Data(:,i) = (Data(:,i) - mean((Data(:,i)).')/std((Data(:,i)).'));
+%end
+sa = [];
+K = [];
+for i = 1:20
+    [idx,c,sumd] = kmeans(Data, i);
+    sa = [sa,sum(sumd)];
+    K = [K,i];
+end
+plot(K, sa)
+
+
+%hacer el clustering
+
+plot(Wines(:,1)), hold on
+[idx,c,sumd] = kmeans(Data, 3);
+plot(idx,'.')
+
+% Se ve que el clustering aun se puede mejorar. Tal vez deberiamos quitar
+% unos datos. Primero vamos a ver la distribucion de cada atributo.
+for i = 2:14
+    boxplot(Wines(:,i),Wines(:,1))
+end
