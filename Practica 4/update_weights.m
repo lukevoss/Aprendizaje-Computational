e529ae6b1682e905d395046e7f1efb429989a9f4
lukevoss <luke@familievoss.net>
2020-11-12 15:31:26 +0100

    updated update_weights

--- Practica 4/update_weights.m
+++ Practica 4/update_weights.m
@@ -2,12 +2,12 @@ function [network] = update_weights(network, row, l_rate)
     fields = fieldnames(network);
     N = length(fields);
     for i = 1:N %iterate through layers
-        inputs = row[1:length(row)-1];
+        inputs = row(1:length(row)-1);
         if i ~= 1
             inputs = network.(fields{i-1}).output;
         end
         layer = network.(fields{i});
-        N_neurons = size(layer.weights, 1)
+        N_neurons = size(layer.weights, 1);
         for neuron = 1:N_neurons
             for j = 1:length(inputs)
                 layer.weights(neuron, j) = layer.weights(neuron, j) + (l_rate * layer.delta(neuron) * inputs(j));
