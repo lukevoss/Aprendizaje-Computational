8919e30a0b230566325b3729f1cb3808b2effd13
lukevoss <luke@familievoss.net>
2020-11-12 13:38:32 +0100

    fixed backwards_propagate_error and tested it a bit

--- Practica 4/backward_propagate_error.m
+++ Practica 4/backward_propagate_error.m
@@ -1,25 +1,27 @@
 function [network] = backward_propagate_error(network, expected)
     fields = fieldnames(network);
-    for i = numel(fields):1 %Go Backwards through network
+    N = length(fields);
+    for i = N:-1:1 %Go Backwards through network with subtracting i from N
         layer = network.(fields{i});
         errors = double.empty;
-        if i ~= (length(fields)-1)
-            for j = 1:length(layer.weights)%count of neurons in this layer
+        if i ~= N
+            for j = 1:size(layer.weights,1)%count of neurons in this layer
                 error = 0.0;
                 layer2 = network.(fields{i+1});
-                for neuron = 1:length(layer2.weigths)
-                    error = error + (layer2.weights(j)*layer2.delta(j));
+                for neuron = 1:size(layer2.weights,1)
+                    error = error + (layer2.weights(neuron, j)*layer2.delta(j));
                 end
-                errors = [errors error];    
+                errors = [errors error];
             end
         else
-            for j = 1:length(layer.weights)
-                errors = [errors (expected(j) - layer.output(j))]
+            for j = 1:size(layer.weights,1)
+                errors = [errors (expected(j) - layer.output(j))];
             end
         end
-        for j = 1:length(layer.weights)
-            layer.delta(j) = errors(j) * transfer_derivate(layer.output(j))
+        for j = 1:size(layer.weights,1)
+            layer.delta(j) = errors(j) * transfer_derivative(layer.output(j));
         end
+        network.(fields{i})= layer;
     end
 end
 
