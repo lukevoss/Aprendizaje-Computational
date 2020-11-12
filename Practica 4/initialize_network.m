6e50339f1986a6c874ca2349f92dc35511bfcad0
Henri <henri.smidt@t-online.de>
2020-11-12 12:44:25 +0100

    delta added

--- Practica 4/initialize_network.m
+++ Practica 4/initialize_network.m
@@ -8,8 +8,10 @@ function network = initialize_network(n_inputs, n_hidden, n_outputs)
 % Returns array with weights
 hidden_layer.weights=rand(n_hidden, n_inputs+1); %+1 for bias
 hidden_layer.output=zeros(1,n_hidden);
+hidden_layer.delta=zeros(1,n_hidden);
 output_layer.weights=rand(n_outputs, n_hidden+1);%+1 for bias
 output_layer.output=zeros(1,n_outputs);
+output_layer.delta=zeros(1,n_outputs);
 network.hidden_layer = hidden_layer;
 network.output_layer = output_layer;
 end
