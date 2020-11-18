54cf8d6dde4ade13e8b0ad06224d5135c3c95dd6
lukevoss <luke@familievoss.net>
2020-11-18 16:52:56 +0100

    Added different distances

--- Practica 5/datos_para_RBF.m
+++ Practica 5/datos_para_RBF.m
@@ -9,6 +9,7 @@ ruido=std(yok);
 figure(1)
 NDATA=10000;
 Xtrain = 8*rand(1,NDATA)-4;
+Xtrain = sort(Xtrain);
 Ytrain=sin(Xtrain).*sin(2*Xtrain)+0.6*ruido*randn(1,NDATA);
 plot(Xtrain,Ytrain,'o');hold on
 plot(x,yok,'.k')
@@ -24,7 +25,7 @@ grid
 
 % B) ENTRENAMIENTO DE LA RBF
 % ==========================
- [idx,centros]=kmeans(Xtrain',NOCULTAS);
+ [idx,centros]=kmedoids(Xtrain',NOCULTAS);
  centros = centros'; %make row vector
  sigma = (max(centros)-min(centros)) /(sqrt(2*NOCULTAS));
  sigma = sigma*ones(1, NOCULTAS);
@@ -32,8 +33,47 @@ grid
 % C) COMPROBACION DEL FUNCIONAMIENTO DE LA RED
 % ============================================
  datos_test = 8*rand(1,NDATA)-4;
+ datos_test = sort(datos_test)
  salida_real = sin(datos_test).*sin(2*datos_test);
  salida_RBF = rbfval(datos_test,centros,sigma,W);
- plot(datos_test,salida_real,'.');hold on;
- plot(datos_test,salida_RBF,'o');hold off;
+ plot(datos_test,salida_real);hold on;
+ plot(datos_test,salida_RBF);
+ 
+ %Mahalanobis distance:
+[idx,centros]=kmedoids(Xtrain',NOCULTAS, 'Distance', 'mahalanobis');
+centros = centros'; %make row vector
+sigma = (max(centros)-min(centros)) /(sqrt(2*NOCULTAS));
+sigma = sigma*ones(1, NOCULTAS);
+W = rbftrain(Xtrain,Ytrain,centros,sigma);
+
+salida_RBF_mahal = rbfval(datos_test,centros,sigma,W);
+plot(datos_test,salida_RBF_mahal,'b');
+
+%Cityblock distance:
+[idx,centros]=kmedoids(Xtrain',NOCULTAS, 'Distance', 'cityblock');
+centros = centros'; %make row vector
+sigma = (max(centros)-min(centros)) /(sqrt(2*NOCULTAS));
+sigma = sigma*ones(1, NOCULTAS);
+W = rbftrain(Xtrain,Ytrain,centros,sigma);
+
+salida_RBF_mahal = rbfval(datos_test,centros,sigma,W);
+plot(datos_test,salida_RBF_mahal,'y');
+
+%Minkowski distance:
+[idx,centros]=kmedoids(Xtrain',NOCULTAS, 'Distance', 'minkowski');
+centros = centros'; %make row vector
+sigma = (max(centros)-min(centros)) /(sqrt(2*NOCULTAS));
+sigma = sigma*ones(1, NOCULTAS);
+W = rbftrain(Xtrain,Ytrain,centros,sigma);
+
+salida_RBF_mahal = rbfval(datos_test,centros,sigma,W);
+plot(datos_test,salida_RBF_mahal,'m');
+
+
+
+legend('sin(x)sin(2x)','Euclidean Distance','Mahalanobis Distance', 'Cityblock Distance', 'Minkowski Distance');
+hold off;
+ 
+ 
+ 
 
