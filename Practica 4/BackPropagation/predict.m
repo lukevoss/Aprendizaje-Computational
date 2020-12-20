9073428c53191120b368768e4019c8ced954bb4f
lukevoss <luke@familievoss.net>
2020-12-20 20:47:37 +0100

    Deleted files

--- Practica 4/BackPropagation/predict.m
+++
@@ -1,4 +0,0 @@
-function outputs = predict(network, row)
-[network, outputs] = forward_propagate(network, row);
-end
-
--- Practica 4/BackPropagation/transfer_derivative.m
+++
@@ -1,3 +0,0 @@
-function output = transfer_derivative(output)
-output = output * (1.0-output);
-end
