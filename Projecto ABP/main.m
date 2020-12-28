c56be2cd54e4729d24605e09db81b065e66e9585
Henri <henri.smidt@t-online.de>
2020-12-28 13:31:51 +0100

    bla

--- Projecto ABP/main.m
+++ Projecto ABP/main.m
@@ -3,14 +3,22 @@ clear all
 close all
 
 %% Cargar conjunto de datos:
-filename = 'breast-cancer-wisconsin.data';
+
+%%Initialize variables.
+filename = 'wdbc.data';
 delimiter = ',';
-formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+
+formatSpec = '%f%C%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
 fileID = fopen(filename,'r');
 dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string',  'ReturnOnError', false);
 fclose(fileID);
-data = [dataArray{2:end}];
+wdbc = table(dataArray{1:end-1}, 'VariableNames', {'ID','Diagnosis','VarName3','VarName4','VarName5','VarName6','VarName7','VarName8','VarName9','VarName10','VarName11','VarName12','VarName13','VarName14','VarName15','VarName16','VarName17','VarName18','VarName19','VarName20','VarName21','VarName22','VarName23','VarName24','VarName25','VarName26','VarName27','VarName28','VarName29','VarName30','VarName31','VarName32'});
+
 clearvars filename delimiter formatSpec fileID dataArray ans;
+
+wdbc.Diagnosis = grp2idx(categorical(wdbc.Diagnosis));
+wdbc = table2array(wdbc);
+wdbc = wdbc(2:end, end)
 
 %% Preprocesimiento de los datos:
 
